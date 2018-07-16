86bc6c62e2c38377956a8529358fe2710f5ac1d2
Jason Hsu <junchoon14@gmail.com>
2018-07-17 00:26:28 +0800

    Britney Spears -Everytime-lyrics

---
+++ Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+func NoticeMe() {
+    print("Notice me")
+    print("Take my hand")
+    print("Why are we")
+    print("Strangers when")
+    print("Our love is strong")
+    print("Why carry on without me")
+}
+
+func EveryTimeITry() {
+    print("Every time I try to fly I fall Without my wings")
+    print("I feel so small")
+    print("I guess I need you baby")
+    print("And every time I see")
+    print("You in my dreams")
+    print("I see your face")
+    print("It's haunting me")
+    print("I guess I need you baby")
+}
+
+func IMakeBelieve() {
+    print("I make-believe")
+    print("That you are here")
+    print("It's the only way")
+    print("That I see clear")
+    print("What have I done")
+    print("You seem to move on easy")
+}
+
+func IMayHaveMadeItRain() {
+print("I may have made it rain")
+print("Please forgive me")
+print("My weakness caused you pain")
+print("And this song's my sorry")
+}
+
+func AtNight() {
+    print("At night I pray")
+    print("That soon your face will fade away")
+}
+
+func EveryTime() {
+    NoticeMe()
+    EveryTimeITry()
+    IMakeBelieve()
+    EveryTimeITry()
+    IMayHaveMadeItRain()
+    EveryTimeITry()
+    AtNight()
+    EveryTimeITry()
+}
+
+EveryTime()
